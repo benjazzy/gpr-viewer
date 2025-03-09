0d2b426148d5c6568990f2f24d90954430deea3b
benjazzy <benjazzy64@gmail.com>
2025-03-08 23:38:57 -0500

    Added aspect option

--- gpr-render.py
+++ gpr-render.py
@@ -1,4 +1,5 @@
 import argparse
+import math
 import os
 from fileinput import filename
 
@@ -39,33 +40,50 @@ def parse_hdf5(file_name):
         min = np.min(bscan)
         max = np.max(bscan)
         vec_normalize = np.vectorize(normalize)
+        return vec_normalize(bscan[:, :], min, max)
 
-        return vec_normalize(bscan[:, :], min, max).repeat(16, axis=1)
 
-
-def parse(file_name):
+def parse(file_name, aspect_ratio):
     if not os.path.isfile(file_name):
         raise FileNotFoundError(file_name)
+
+    bscan = None
 
     extension = os.path.splitext(file_name)[1]
     if extension == ".ASC":
         print("Reading ASC")
-        return parse_asc(file_name)
+        bscan = parse_asc(file_name)
     elif extension == ".out":
         print("Reading hdf5")
-        return parse_hdf5(file_name)
+        bscan = parse_hdf5(file_name)
     else:
         print(f"Unkown extension: {extension}")
         raise ValueError
+
+    if aspect_ratio:
+        data_height, data_width = bscan.shape
+        data_ratio = data_width / data_height
+        print(data_ratio)
+        ratio_multiplier = round(aspect_ratio / data_ratio)
+        print(ratio_multiplier)
+        if ratio_multiplier >= 2:
+            bscan = bscan.repeat(ratio_multiplier, axis=1)
+
+    return bscan
 
 
 def main():
     parser = argparse.ArgumentParser(prog="gpr-render")
     parser.add_argument("filename")
+    parser.add_argument("-a", "--aspect-ratio", required=False)
     args = parser.parse_args()
     name = os.path.splitext(args.filename)[0]
+    aspect_ratio = None
+    if args.aspect_ratio:
+        width, height = args.aspect_ratio.split(":")
+        aspect_ratio = float(width) / float(height)
 
-    bscan = parse(args.filename)
+    bscan = parse(args.filename, aspect_ratio)
     print(f"Writing output to {name}.png")
     image = Image.fromarray(bscan, "L")
     image.save(f"{name}.png")
